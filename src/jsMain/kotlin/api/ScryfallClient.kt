8fe859534bde3477e2b4d149011a429aa91ca516
Jesse <jessemckee97@gmail.com>
2023-04-16 14:55:51 -0500

    Revert to exact until I implement fuzzy searching.

--- src/jsMain/kotlin/api/ScryfallClient.kt
+++ src/jsMain/kotlin/api/ScryfallClient.kt
@@ -20,7 +20,7 @@ class ScryfallClient(private val client: HttpClient, private val url: String = "
     private suspend fun fetchNamed(input: String): HttpResponse {
         return fetch("$url/named") {
             url {
-                parameters.append("fuzzy", input)
+                parameters.append("exact", input)
             }
         }
     }
